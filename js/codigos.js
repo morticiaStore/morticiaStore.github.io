4d3ec611de9bd67fffcbb1a79cebb9de0d3d6cb9
morticiaStore <77864933+morticiaStore@users.noreply.github.com>
2024-02-06 00:24:47 -0300

    Add files via upload

--- js/codigos.js
+++ js/codigos.js
@@ -1,7 +1,35 @@
+function showDropBarCategorias(){
+	$("#dropBarCategorias > img").css("display","none");
+	$("#dropBarCategorias > nav").css("display","inline");
+}
 
-function showDropDownContent(){
+function hideDropBarCategorias(){
+	$("#dropBarCategorias > img").css("display","inline");
+	$("#dropBarCategorias > nav").css("display","none");
+}
 
-	$("#dropBarCategorias img").css("display","hidden");
+function showSearchBar(){
+	$("#searchBar").css("display","flex");
+}
+
+function hideSearchBar(){
+	$("#searchBar").css("display","none");
 
 }
 
+function addCarrinho(){
+
+	function x(){
+		return Number($("#numItemsCarrinho").text().split("")[1])+1
+	}
+
+	$("#numItemsCarrinho").text("("+ x() +")");
+}
+
+function showUserBox(){
+	$("#userBox").css("display", "flex");
+}
+
+function hideUserBox(){
+	$("#userBox").css("display", "none");
+}
